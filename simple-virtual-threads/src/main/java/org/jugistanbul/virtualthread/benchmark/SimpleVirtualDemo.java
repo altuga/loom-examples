be8d39a2da8d6fdaa22f520cfd47329ab9d895e2
Altuğ Bilgin ALTINTAŞ <altuga@gmail.com>
2024-01-03 14:52:47 +0000

    kata

---
+++ simple-virtual-threads/src/main/java/org/jugistanbul/virtualthread/benchmark/SimpleVirtualDemo.java
@@ -0,0 +1,26 @@
+package org.jugistanbul.virtualthread.benchmark;
+
+import org.jugistanbul.util.ThreadType;
+import org.jugistanbul.util.ThreadUtil;
+
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+/**
+ * @author Altug (altuga@gmail.com)
+ *
+ ***/
+public class SimpleVirtualDemo {
+
+    public static void main(String[] args) throws InterruptedException{
+         Runnable printTask = ()-> System.out.println("Hello Istanbul ...");
+         Runnable sleepTask = () -> System.out.println("...slepping ..." );   
+        
+         Thread vt = Thread.startVirtualThread(printTask);
+         vt.join();
+    }
+
+    
+}
