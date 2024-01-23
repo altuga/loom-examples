fe3aa7a6e2f98aeddee51ff1e8aa24a5e05cd3b0
Altuğ Bilgin ALTINTAŞ <altuga@gmail.com>
2024-01-23 08:30:38 +0000

    kata

--- simple-virtual-threads/src/main/java/org/jugistanbul/virtualthread/benchmark/SimpleVirtualDemo.java
+++ simple-virtual-threads/src/main/java/org/jugistanbul/virtualthread/benchmark/SimpleVirtualDemo.java
@@ -17,9 +17,10 @@ public class SimpleVirtualDemo {
 
     public static void main(String[] args) throws InterruptedException {
         Runnable printTask = () -> System.out.println("Hello Istanbul *** ");
-        
+        Runnable task = () -> System.out.println(Thread.currentThread());
+        // create a new Thread 
 
-        Thread vt = Thread.ofVirtual().name("vt").unstarted(printTask);
+        Thread vt = Thread.ofVirtual().unstarted(task);
         vt.start();
         vt.join();
 
